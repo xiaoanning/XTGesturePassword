89b2301460834638b1ce27ac309982e48e8e1a58
xiaoanning <734871221@qq.com>
2017-05-13 11:16:20 +0800

    Initial Commit

---
+++ GesturePassword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GesturePassword
+//
+//  Created by 安宁 on 2017/5/13.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GesturePassword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturePassword
+//
+//  Created by 安宁 on 2017/5/13.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
